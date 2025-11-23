1e3d27f15ebac5358b3eff8cb6618a46ef53343a
nur-nayeem <nurnayem768@gmail.com>
2025-11-23 16:21:13 +0600

    update secure axios hook

--- src/hook/useAxiosSecure.js
+++ src/hook/useAxiosSecure.js
@@ -1,12 +1,46 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
-const instance = axios.create({
+const axiosSecure = axios.create({
   baseURL: "http://localhost:4000",
-  // timeout: 1000,
-  // headers: { "X-Custom-Header": "foobar" },
 });
 
 const useAxiosSecure = () => {
-  return instance;
+  const { user, signOutUser } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //request:
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+
+    //response
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          signOutUser().then(() => {
+            navigate("/login");
+          });
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, signOutUser, navigate]);
+
+  return axiosSecure;
 };
 export default useAxiosSecure;
